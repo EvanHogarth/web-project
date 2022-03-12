fb2a8a947687696690ef251a536b8f8ba7c46d5f
Evan Hogarth <evanhogarth@Evans-MacBook-Pro-2.local>
2022-03-12 15:35:36 -0600

    Initializing starter files

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Shop</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <nav>
+      <a href="index.php"><img id="logo" src="images/logo.svg" alt="logo"></a>
+      <a class="nav-links" href="#">Collections</a>
+      <a class="nav-links" href="#">Men</a>
+      <a class="nav-links" href="#">Women</a>
+      <a class="nav-links" href="#">About</a>
+      <a class="nav-links" href="#">Contact</a>
+
+      <img id="cart-icon" src="images/icon-cart.svg" alt="cart">
+    </nav>
+
+    <div class="product-page">
+
+      <div class="product-images">
+        <img class="product-image" src="images/image-product-1.jpg" alt="product">
+      </div>
+
+      <div class="product-info">
+        <h3>Sneaker Company</h3>
+        <h2>Fall Limited Edition Sneakers</h2>
+        <p>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything the
+          weather can offer.
+        </p>
+        <p class="product-price">$125.00</p>
+        <button type="button" name="button">Add to cart</button>
+      </div>
+
+    </div>
+
+    <footer>
+      <p>Evan Hogarth</p>
+    </footer>
+
+  </body>
+</html>
