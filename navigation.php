31bfbc09263a5cf6ae5f22296323681f286b6831
Evan Hogarth <evanhogarth8@gmail.com>
2022-03-22 18:26:47 -0500

    Changes to logo and navigation.

--- navigation.php
+++ navigation.php
@@ -7,13 +7,14 @@
 ?>
 
 <nav>
-  <a href="index.php"><img id="logo" src="images/logo.svg" alt="logo"></a>
+  <a href="index.php"><h2 id="logo">Logo</h2></a>
+  <!-- <a href="index.php"><img id="logo" src="images/logo.svg" alt="logo"></a> -->
 
   <?php while($category = $navStatement->fetch()): ?>
     <a class="nav-links" href="category_page.php?id=<?= $category['category_id'] ?>"><?= $category['category_name'] ?></a>
   <?php endwhile ?>
 
-  <a class="nav-links" href="admin.php">CMS</a>
+  <a id="admin-link" class="nav-links" href="admin.php">CMS</a>
 
   <img id="cart-icon" src="images/icon-cart.svg" alt="cart">
 </nav>
