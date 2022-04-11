62315bbce21ad6ba0c947823061f671e20c36efe
Evan Hogarth <evanhogarth8@gmail.com>
2022-04-11 13:47:32 -0500

    Added search bar and all products page.

--- navigation.php
+++ navigation.php
@@ -10,9 +10,31 @@
   <a href="index.php"><h2 id="logo">Logo</h2></a>
   <!-- <a href="index.php"><img id="logo" src="images/logo.svg" alt="logo"></a> -->
 
+  <a class="nav-links" href="category_page.php?id=<?= $category['category_id'] ?>">All Products</a>
   <?php while($category = $navStatement->fetch()): ?>
     <a class="nav-links" href="category_page.php?id=<?= $category['category_id'] ?>"><?= $category['category_name'] ?></a>
   <?php endwhile ?>
+
+  <form id="search-bar" action="search_products.php">
+    <input id="search-input" type="text" placeholder="Search..." name="search">
+
+    <?php
+      $category_query = "SELECT * FROM categories";
+      $category_statement = $db->prepare($category_query);
+      $category_statement->execute();
+      $categories = $category_statement->fetchAll();
+    ?>
+    <select id="category-search" name="category">
+      <option selected="selected" value="-1">All Categories</option>
+      <?php foreach($categories as $category): ?>
+        <option value="<?= $category['category_id'] ?>"><?= $category['category_name'] ?></option>
+      <?php endforeach ?>
+    </select>
+
+    <button id="search-button" type="submit"><img id="search-icon" src="images/search-symbol.png" alt=""></button>
+  </form>
+
+
 
   <a id="admin-link" class="nav-links" href="admin.php">CMS</a>
 
