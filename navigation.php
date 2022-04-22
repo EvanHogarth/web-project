69212b76cd97dc4e4e7280e42724353c69cffd06
Evan Hogarth <evanhogarth8@gmail.com>
2022-04-22 17:58:01 -0500

    Changed categories to drop down, added log in and log out feature.

--- navigation.php
+++ navigation.php
@@ -1,4 +1,6 @@
 <?php
+  session_start();
+
   $navQuery = "SELECT * FROM categories";
 
   $navStatement = $db->prepare($navQuery);
@@ -7,13 +9,21 @@
 ?>
 
 <nav>
-  <a href="index.php"><h2 id="logo">Logo</h2></a>
+  <a href="index.php"><h2 id="logo">Silver Lotus</h2></a>
   <!-- <a href="index.php"><img id="logo" src="images/logo.svg" alt="logo"></a> -->
 
-  <a class="nav-links" href="all_products.php">All Products</a>
-  <?php while($category = $navStatement->fetch()): ?>
-    <a class="nav-links" href="category_page.php?id=<?= $category['category_id'] ?>"><?= $category['category_name'] ?></a>
-  <?php endwhile ?>
+  <div class="dropdown">
+    <button class="dropdown-button">Categories ▼</button>
+    <div class="dropdown-content">
+      <!-- <?php if(isset($_SESSION["logged_in_email"])): ?>
+        <a class="nav-links" href="all_products.php">All Products</a>
+      <?php endif ?> -->
+      <a class="nav-links" href="all_products.php">All Products</a>
+      <?php while($category = $navStatement->fetch()): ?>
+        <a class="nav-links" href="category_page.php?id=<?= $category['category_id'] ?>"><?= $category['category_name'] ?></a>
+      <?php endwhile ?>
+    </div>
+  </div>
 
   <form id="search-bar" action="search_products.php">
     <input id="search-input" type="text" placeholder="Search..." name="search">
@@ -34,7 +44,11 @@
     <button id="search-button" type="submit"><img id="search-icon" src="images/search-symbol.png" alt=""></button>
   </form>
 
-
+  <?php if(isset($_SESSION["logged_in_email"])): ?>
+    <a class="nav-links" href="logout.php">Log Out</a>
+  <?php else: ?>
+    <a class="nav-links" href="login_page.php">Log In</a>
+  <?php endif ?>
 
   <a id="admin-link" class="nav-links" href="admin.php">CMS</a>
 
