ffa2c9c81792d39df4ee37a9777bf873312c07ec
Evan Hogarth <evanhogarth8@gmail.com>
2022-04-25 00:03:23 -0500

    Simple category selection on homepage.

--- index.php
+++ index.php
@@ -14,11 +14,20 @@
     <?php require('navigation.php') ?>
 
     <main class="homepage-content">
-
-      <h3>New Arrivals</h3>
-      <div class="categories-homepage" style="background-image: url(images/black-buddha-bracelet.jpg); ">
-        <button class="shop-button" type="button" name="button">Shop</button>
-      </div>
+      <h2>Welcome!</h2>
+      <h3>Shop by Category</h3>
+      <?php
+        $category_query = "SELECT * FROM categories";
+        $category_statement = $db->prepare($category_query);
+        // $statement->bindValue(':id', $id, PDO::PARAM_INT);
+        $category_statement->execute();
+        $categories = $category_statement->fetchAll();
+      ?>
+      <?php foreach($categories as $category): ?>
+        <div>
+          <h4><a href="category_page.php?id=<?= $category['category_id'] ?>"><?= $category['category_name'] ?></a></h4>
+        </div>
+      <?php endforeach ?>
     </main>
 
     <footer>
