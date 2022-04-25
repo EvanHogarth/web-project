6faecbac96378d172082e258e58cfe2808ce3b87
Evan Hogarth <evanhogarth8@gmail.com>
2022-04-25 00:03:53 -0500

    Added permalink slug text into the URL.

--- product_page.php
+++ product_page.php
@@ -2,16 +2,24 @@
   require('connect.php');
 
   $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+  $slug = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-  $query = "SELECT * FROM products WHERE id = :id";
+  $slug = str_replace("-", " ", $slug);
+
+  $query = "SELECT * FROM products WHERE id = :id AND product_name = :slug";
 
   $statement = $db->prepare($query);
 
   $statement->bindValue(':id', $id, PDO::PARAM_INT);
+  $statement->bindValue(':slug', $slug);
 
   $statement->execute();
 
   $row = $statement->fetch();
+
+  if(empty($row)) {
+    $error_msg = "Sorry no product was found.";
+  }
 
   //Handle comment form submission
   if($_POST && isset($_POST['submit']) && !empty($_POST['comment'])){
@@ -19,6 +27,7 @@
     $product_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
     $user_name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $review_comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $slug = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
     // Create INSERT query
     $query = "INSERT INTO reviews (product_id, user_name, comment) values (:product_id, :user_name, :comment)";
@@ -32,7 +41,7 @@
     // Execute statement
     $statement->execute();
 
-    header("Location:product_page.php?id=<?= $product_id ?>");
+    header("Location:product_page.php?id=".$product_id."&p=".str_replace("%20", " ", $slug));
   }
 ?>
 
@@ -47,72 +56,73 @@
     <?php require('navigation.php') ?>
 
     <div class="product-page">
+      <?php if(isset($error_msg)): ?>
+        <h2><?= $error_msg ?></h2>
 
-      <?php if(!empty($row['image_url'])): ?>
-        <div class="product-images">
-          <img class="product-image" src="<?= $row['image_url'] ?>" alt="product image">
-        </div>
-      <?php endif ?>
-
-      <div class="product-info">
-        <!-- Category -->
-        <h3>Bracelets</h3>
-
-        <!-- Product Name -->
-        <h2><?= $row['product_name'] ?></h2>
-
-        <!-- Product Description -->
-        <p><?= $row['description'] ?></p>
-
-        <!-- Product Price -->
-        <p class="product-price">$<?= $row['price'] ?></p>
-
-        <!-- If stock is greater than 0, allow to add to cart -->
-        <?php if($row['stock'] > 0): ?>
-          <button type="button" name="button">Add to cart</button>
-        <?php else: ?>
-          <h4>Out of stock</h4>
-        <?php endif ?>
-      </div>
-
-    </div>
-
-    <div class="comment-container">
-      <h4>Comments</h4>
-
-      <?php
-        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        $comment_query = "SELECT * FROM reviews WHERE product_id = :id ORDER BY created_on DESC";
-        $comment_statement = $db->prepare($comment_query);
-        $comment_statement->bindValue(':id', $id, PDO::PARAM_INT);
-        $comment_statement->execute();
-        $comments = $comment_statement->fetchAll();
-      ?>
-      <?php if(empty($comments)): ?>
-        <p>There are no reviews for this product.</p>
       <?php else: ?>
-        <?php foreach($comments as $comment): ?>
-          <div class="comment-block">
-            <h5>Name: <?= $comment['user_name'] ?></h5>
-            <p>Created on: <?= $comment['created_on'] ?></p>
-            <p>Comment: <?= $comment['comment'] ?></p>
+
+            <?php if(!empty($row['image_url'])): ?>
+              <div class="product-images">
+                <img class="product-image" src="<?= $row['image_url'] ?>" alt="product image">
+              </div>
+            <?php endif ?>
+
+            <div class="product-info">
+              <h2><?= $row['product_name'] ?></h2>
+              <p><?= $row['description'] ?></p>
+              <p class="product-price">$<?= $row['price'] ?></p>
+
+              <!-- If stock is greater than 0, allow to add to cart -->
+              <?php if($row['stock'] > 0): ?>
+                <button type="button" name="button">Add to cart</button>
+              <?php else: ?>
+                <h4>Out of stock</h4>
+              <?php endif ?>
+            </div>
+
           </div>
-        <?php endforeach ?>
-      <?php endif ?>
+
+          <div class="comment-container">
+            <h4>Comments</h4>
+
+            <?php
+              $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+              $comment_query = "SELECT * FROM reviews WHERE product_id = :id ORDER BY created_on DESC";
+              $comment_statement = $db->prepare($comment_query);
+              $comment_statement->bindValue(':id', $id, PDO::PARAM_INT);
+              $comment_statement->execute();
+              $comments = $comment_statement->fetchAll();
+            ?>
+            <?php if(empty($comments)): ?>
+              <p>There are no reviews for this product.</p>
+            <?php else: ?>
+              <?php foreach($comments as $comment): ?>
+                <div class="comment-block">
+                  <h5>Name: <?= $comment['user_name'] ?></h5>
+                  <p>Created on: <?= $comment['created_on'] ?></p>
+                  <p>Comment: <?= $comment['comment'] ?></p>
+                </div>
+              <?php endforeach ?>
+            <?php endif ?>
 
 
-      <form id="comment-form" class="comment-form" action="" method="post">
-        <h4>Add a comment</h4>
+            <form id="comment-form" class="comment-form" action="" method="post">
+              <h4>Add a comment</h4>
 
-        <label for="user_name">Name:</label>
-        <input id="user_name" type="text" name="user_name" value="">
+              <label for="user_name">Name:</label>
+              <input id="user_name" type="text" name="user_name" value="">
 
-        <label for="comment">Comment:</label>
-        <textarea id="comment" name="comment" rows="8" cols="80"></textarea>
+              <label for="comment">Comment:</label>
+              <textarea id="comment" name="comment" rows="8" cols="80"></textarea>
 
-        <button type="submit" name="submit">Submit</button>
-      </form>
+              <button type="submit" name="submit">Submit</button>
+            </form>
+
+
+    <?php endif ?>
     </div>
+
+
 
     <footer>
       <p>Evan Hogarth</p>
